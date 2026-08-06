bee39fdbdc1acf6412343171bb7bce9c34b64dc2
Aluno.Etec <aula@etec.com>
2026-08-06 08:48:37 -0300

    instalacao do bootstrap

--- index.php
+++ index.php
@@ -3,9 +3,69 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página =inicial</title>
+    <title>Página inicial</title>
+       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme= dark>
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="https://static.wikia.nocookie.net/sanrio/images/9/9f/Hello_Kitty.jpg/revision/latest/scale-to-width-down/985?cb=20191128154539" alt="Bootstrap" width="30" height="24">
+
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Cadastros</a>
+        </li>
+         <li class="nav-item">
+          <a class="nav-link" href="#">Relatórios</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Selecione a opção
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Usuários</a></li>
+            <li><a class="dropdown-item" href="#">Produtos</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Clientes</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
     <h1> Bem vindo ao sistema </h1>
+    <button type="button" class="btn btn-success">Success
+        Salvar 
+    </button>
+    <div class="alert alert-danger" role="alert">
+        Cuidado! Sua nota vai ser I
+    </div>
+    <div class="card">
+  <div class="card-header bg-primary">
+    <span class="text-white"> <strong> Pesquisar</strong></span>
+  </div>
+  <div class="card-body">
+    <p class="card-text">Aqui será um menu de pesquisa</p>
+    <a href="#" class="btn btn-primary">Pesquisar</a>
+  </div>
+</div>
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
