1cc9cfcdffc4d573d32260ed3b5b96618a117486
Laboratorio <laboratorio@escola.local>
2026-08-19 10:45:04 -0300

    Validação do username

--- cabecalho.php
+++ cabecalho.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.css" />]
+    <link rel="stylesheet" href="css/reset.css" /> 
 </head>
 <body>
 <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
@@ -31,6 +32,6 @@
     </div>
   </div>
 </nav>
-<div class="container">
+<div class="container h-75 d-flex align-items-center">
 
 
--- index.php
+++ index.php
@@ -1,6 +1,20 @@
 <?php include "cabecalho.php"; ?>
+<?php
+if(isset($_POST["login"]) )
+  {
+    if(!empty($_POST["login"]) )
+      {
+        $mensagemErro = "Preencha os campos de login";
+      }
+    //Procedimento para banco de dados 
+  }
+else 
+  {
+    
+  }
+?>
 
-<div class="row">
+<div class="row w-100">
   <div class="col-md-4">
 
   </div>
@@ -9,14 +23,28 @@
 
       <div class="card">
         <div class="card-body">
+
+        <?php
+        if(isset($mensagemErro) && !empty($mensagemErro))
+          {
+          echo '<div class="alert alert-danger">'
+            echo $mensagemErro;
+            echo '</div>';
+        
+          }
+        ?>
           <form action="" method="post">
               <label for="login">Username</label>
               <input class="form-control" type="text" name="login" id="login"/>
               <label for="senha">Senha</label>
               <input class="form-control" type="password" name="senha" id="senha"/>
-              <div class="row>
-                <div class="col-mds-6></div>
-                <div class="col-mds-6></div>
+            <div class="row mt-3">
+                <div class="col-md-6">
+                  <button type="submit" class="btn btn-primary"> Entrar </button>
+                  </div>
+                <div class="col-md-6 d-flex align-items-center d-flex justify-content-end">
+                  <input type="checkbox" class="form-check-input mx-2" value="senha"/> Salvar senha
+              </div>
             </div>
           </form>
         </div>
